7786438d162add6386d0347da3722ee39ac03356
toshiakiasakura <aflu.blossompaws@gmail.com>
2020-12-07 15:47:05 +0900

    Aligne space was adjusted.

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -9,8 +9,9 @@ function Home() {
       <div className="hero">
         <div className="hero__bg" />
         <div className="hero__welcome">
-          <h1 className="hero__welcome__title">北医ネット(作成中) </h1>
+          <div className="hero__welcome__title">北医ネット<br/>(作成中) </div>
           <div className="hero__welcome__elocution">
+           {/*TO DO: Connect with backend and display notification. */}
             <h3> お知らせ </h3>
             <table>
               <tr>
@@ -19,12 +20,12 @@ function Home() {
               </tr>
             </table>
           </div>
+          <h2> テスト </h2>
+          <form name="login-form"> <br/>
+            <input type="email" name="email" /><br/>
+            <input type="password" name="password" />
+          </form>
         </div>
-        <h2> テスト </h2>
-        <form name="login-form"> <br/>
-          <input type="email" name="email" /><br/>
-          <input type="password" name="password" />
-        </form>
       </div>
     </div>
   );
