8806623c250055fd04ea9ab5c4c26cba7a8d6ed5
toshiakiasakura <aflu.blossompaws@gmail.com>
2020-12-19 16:07:14 +0900

    API_URL was rewritten. Ommit localhost part.

--- client/src/services/admin.service.ts
+++ client/src/services/admin.service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { authHeader } from './auth-header'
-const API_URL = 'http://localhost:3000/api/admin/'
+const API_URL = '/api/admin/'
 
 
 export interface Users {
